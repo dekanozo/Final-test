ad17eaaf6d6eb190136b33d3cd6fece164f7a48c
Timofey Dekanozo <dekanozov@gmail.com>
2024-05-11 14:03:56 +0300

    написали код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод массива строк с клавиатуры
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+        string[] originalArray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Формирование нового массива строк
+        string[] newArray = FilterArray(originalArray);
+
+        // Вывод нового массива на экран
+        Console.WriteLine("Новый массив строк:");
+        foreach (string str in newArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static string[] FilterArray(string[] array)
+    {
+        // Подсчет количества элементов, удовлетворяющих условию
+        int count = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива нужного размера
+        string[] result = new string[count];
+        int index = 0;
+
+        // Заполнение нового массива строками, удовлетворяющими условию
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                result[index] = str;
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
+
